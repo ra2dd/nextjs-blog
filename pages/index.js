cbb19a522b1baa94f9ae2a84fa22f01a435392ce
ra2dd <radoslawpryw@gmail.com>
2023-08-28 12:39:23 +0200

    fetch post data and display it on site with ssg

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,18 @@
 import Head from 'next/head';
 import Layout, {siteTitle} from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
+import { getSortedPostsData } from '../lib/posts';
 
-export default function Home() {
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
@@ -10,12 +20,27 @@ export default function Home() {
       </Head>
       <section className={utilStyles.headingMd}>
         <p>
-          Hello. I'm creating this website, because I wanted to learn Next.js framework.
-          Up to now I've been really enjoying learning the fundamentals.
+          Hello. I created this website, because I'm learning Next.js framework.
+          Up to now I've been enjoying learning the fundamentals.
           </p>
         <p>
           (This website was made with Next.js)
         </p>
+      </section>
+
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title}) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   );
